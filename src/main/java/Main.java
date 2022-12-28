924363adcf5f6c60e7797598e18756669394be8c
BuildTools <85534497+Shinkume@users.noreply.github.com>
2022-12-28 15:31:10 -0500

    adding listeners

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,27 @@
 import com.theokanning.openai.OpenAiService;
+import com.theokanning.openai.moderation.ModerationRequest;
+import com.theokanning.openai.moderation.ModerationResult;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.message.GenericMessageEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
 
+import javax.security.auth.login.LoginException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.EventListener;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends ListenerAdapter {
 
-    public static void main(String[] args) throws FileNotFoundException
+    private static JDABuilder builder;
+    private static Guild guild;
+
+    public static void main(String[] args) throws FileNotFoundException, LoginException
     {
        File file = new File("tokens.txt");
         Scanner sc = new Scanner(file);
@@ -19,7 +32,19 @@ public class Main {
         String openaikey = line2.substring(10,line2.length()-1);
         System.out.println(openaikey);
         OpenAiService service = new OpenAiService(openaikey);
-        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(discordtoken);
+        builder = JDABuilder.createDefault(discordtoken);
+        builder.build();
+
+        guild.upsertCommand("chat","starts a new conversation with the openai chatbot!");
+       /*ModerationRequest request = new ModerationRequest();
+       ModerationResult result = new ModerationResult();*/
+
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+
     }
 
 }
