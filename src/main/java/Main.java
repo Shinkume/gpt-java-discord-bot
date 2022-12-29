c97242ce2093e2f22b24ad2a896ff9bb515d7365
BuildTools <85534497+Shinkume@users.noreply.github.com>
2022-12-29 16:02:16 -0500

    add option to slash command and more

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,32 @@
 import com.theokanning.openai.OpenAiService;
+import com.theokanning.openai.moderation.Moderation;
+import com.theokanning.openai.moderation.ModerationCategories;
 import com.theokanning.openai.moderation.ModerationRequest;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.Command;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
 import javax.security.auth.login.LoginException;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main extends ListenerAdapter {
-
+    private static String thread;
     private static JDABuilder builder;
     private boolean chatting = false;
-    public static void main(String[] args) throws FileNotFoundException, LoginException
+    private static OpenAiService service;
+    public static void main(String[] args) throws IOException, LoginException
     {
        File file = new File("tokens.txt");
         Scanner sc = new Scanner(file);
@@ -25,30 +36,50 @@ public class Main extends ListenerAdapter {
         System.out.println(discordtoken);
         String openaikey = line2.substring(10,line2.length()-1);
         System.out.println(openaikey);
-        OpenAiService service = new OpenAiService(openaikey);
+        service = new OpenAiService(openaikey);
         builder = JDABuilder.createDefault(discordtoken);
         builder.build();
+        File file1 = new File("config.yaml");
+        Scanner scanner = new Scanner(file1);
+        for(int i = 0; i < Files.lines(Path.of("prompt.txt")).count();i++)
+        {
+            thread += scanner.next();
+        }
 
-       ModerationRequest request = new ModerationRequest();
 
     }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event)
     {
+        if(chatting)
+        {
+            //reminder to add moderation and openai completion
+            chatting = true;
+            String message = event.getMessage().getContentRaw();
+            thread += event.getAuthor() + "#" + event.getAuthor().getAsTag() + ": " + message + "\n";
+            thread += "You: ";
 
+
+        }
     }
 
     @Override
     public void onGuildReady(GuildReadyEvent event) {
-        event.getGuild().upsertCommand("chat","Allows the user to interact with the gpt-3 chatbot!").queue();
+        event.getGuild().upsertCommand(Commands.slash("chat","Start a conversation with the gpt-3 chatbot!").
+                addOption(OptionType.STRING, "message","The message you want to send to the chatbot!")).queue();
     }
 
     @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
-        if(event.getCommandString().equals("chat"))
+        if(event.getName().equals("chat"))
         {
+            //reminder to add moderation and openai completion
 
+            chatting = true;
+            String message = event.getOption("message").getAsString();
+            thread += event.getUser() + "#" + event.getUser().getAsTag() + ": " + message + "\n";
+            thread += "You: ";
         }
     }
 
