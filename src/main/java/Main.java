65dc4e1dbbf0b4f241fc54b4d960819595717310
BuildTools <85534497+Shinkume@users.noreply.github.com>
2022-12-30 12:33:08 -0500

    added moderation and completion

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,16 @@
 import com.theokanning.openai.OpenAiService;
+import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.moderation.Moderation;
-import com.theokanning.openai.moderation.ModerationCategories;
 import com.theokanning.openai.moderation.ModerationRequest;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
 import javax.security.auth.login.LoginException;
 import java.io.File;
@@ -19,6 +18,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main extends ListenerAdapter {
@@ -26,7 +27,7 @@ public class Main extends ListenerAdapter {
     private static JDABuilder builder;
     private boolean chatting = false;
     private static OpenAiService service;
-    public static void main(String[] args) throws IOException, LoginException
+    public static void main(String[] args) throws IOException, LoginException, FileNotFoundException
     {
        File file = new File("tokens.txt");
         Scanner sc = new Scanner(file);
@@ -46,20 +47,36 @@ public class Main extends ListenerAdapter {
             thread += scanner.next();
         }
 
-
     }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event)
     {
-        if(chatting)
+        if(chatting && event.getChannel() instanceof  ThreadChannel)
         {
+            if(!(event.getChannel().asThreadChannel().getName().equals("chat"))) return;
             //reminder to add moderation and openai completion
-            chatting = true;
             String message = event.getMessage().getContentRaw();
-            thread += event.getAuthor() + "#" + event.getAuthor().getAsTag() + ": " + message + "\n";
-            thread += "You: ";
-
+            ModerationRequest request = new ModerationRequest();
+            request.setInput(message);
+            Moderation moderation = service.createModeration(request).getResults().get(0);
+            if(moderation.isFlagged())
+            {
+                event.getMessage().delete().queue();
+                event.getMessage().reply("Your content was flagged by moderation").queue();
+                return;
+            }else
+            {
+                thread += event.getAuthor() + "#" + event.getAuthor().getAsTag() + ": " + message + "\n";
+                thread += "You: ";
+                CompletionRequest completionRequest = new CompletionRequest();
+                completionRequest.setPrompt(thread);
+                completionRequest.setMaxTokens(250);
+                completionRequest.setModel("davinci-003");
+                completionRequest.setTemperature(0.9);
+                String response =  service.createCompletion(completionRequest).getChoices().get(0).getText();
+                event.getChannel().sendMessage(response).queue();
+            }
 
         }
     }
@@ -71,15 +88,58 @@ public class Main extends ListenerAdapter {
     }
 
     @Override
+    public void onGuildJoin(GuildJoinEvent event) {
+        event.getGuild().upsertCommand(Commands.slash("chat","Start a conversation with the gpt-3 chatbot!").
+                addOption(OptionType.STRING, "message","The message you want to send to the chatbot!")).queue();
+    }
+
+    @Override
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         if(event.getName().equals("chat"))
         {
             //reminder to add moderation and openai completion
-
             chatting = true;
             String message = event.getOption("message").getAsString();
-            thread += event.getUser() + "#" + event.getUser().getAsTag() + ": " + message + "\n";
-            thread += "You: ";
+            ModerationRequest request = new ModerationRequest();
+            request.setInput(message);
+            Moderation moderation = service.createModeration(request).getResults().get(0);
+                if(moderation.isFlagged())
+                  {
+                      event.reply("Your content was blocked for being offensive").setEphemeral(true);
+                      return;
+                  }else if(!(event.getChannel() instanceof  ThreadChannel ))
+                  {
+                      for(ThreadChannel thread : event.getChannel().asTextChannel().getThreadChannels())
+                      {
+                          if(thread.getName().equals("chat"))
+                          {
+                              event.reply("I am already in a thread, sorry!").setEphemeral(true).queue();
+                              return;
+                          }
+                      }
+                      event.getChannel().asTextChannel().createThreadChannel("chat").queue();
+                      thread += event.getUser() + "#" + event.getUser().getAsTag() + ": " + message + "\n";
+                      thread += "You: ";
+                      CompletionRequest completionRequest = new CompletionRequest();
+                      completionRequest.setPrompt(thread);
+                      completionRequest.setMaxTokens(250);
+                      completionRequest.setModel("davinci-003");
+                      completionRequest.setTemperature(0.9);
+                      String response =  service.createCompletion(completionRequest).getChoices().get(0).getText();
+                      ThreadChannel channel = null;
+                      for(ThreadChannel thread : event.getChannel().asTextChannel().getThreadChannels())
+                      {
+                          if(thread.getName().equals("chat"))
+                          {
+                             channel = thread;
+                          }
+                      }
+                     if(channel != null) channel.sendMessage(response).queue();
+                  }else
+                  {
+                      event.reply("I am already in a thread, sorry!").setEphemeral(true).queue();
+                      return;
+                  }
         }
     }
 
